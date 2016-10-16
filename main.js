4b4c65655fd8ba33f6f051727e6c5d5c9551e97d
Brett MacDonald <macdonald.brett@outlook.com>
2016-10-16 15:10:21 -0700

    2nd commit after all build files lost on previous commit
    on last commit forgot to add node files to be ignored lost gulp file
    and minified js files

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
-/**
- * Created by brettmacdonald on 2016-10-12.
- */
+
+$(function() {
+  $('select').on('change', function() {
+
+    $('img').toggleClass("small-logo", "large-logo", 1000, "easeOutQuad");
+    $('header').toggleClass("site-header-small", "site-header-large", 1000, "easeOutQuad");
+
+
+    var select = $('#sections').val();
+    console.log(select)
+
+    $.ajax({
+
+      method: 'GET',
+      url: 'https://api.nytimes.com/svc/topstories/v2/' + select + '.json?api-key=133064f593064b5a9b82e24190084a92'
+    }).done(function(data) {
+
+if (data.results.length === 0) {
+          $('.story-grid').append("<p>Sorry, nothing found! Please try again.</p>");
+        }
+        else {
+          var news = data.results;
+          news = news.filter(function(item) {
+            return item.multimedia.length;
+          }).splice(0, 12);
+
+            news.forEach(function(item, index) {
+            $('.stories ul').append('<li class="news' + index +'"><div class="inner-item-wrapper"><a href="' +
+              item.url + '"><div class="article story-' + index +'"><div class="story-meta"><p>' + item.abstract + '</p></div></div></a></div></li>');
+
+
+
+
+            img = item.multimedia[4];
+            $('.story-' + index).css('background-image', 'url("' + img.url + '")');
+          });
+
+};
+
+
+
+});
+});
+});
+
+
