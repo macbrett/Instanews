426278b9a1eb0163f2fa868b17e32792ff6a59a1
Brett MacDonald <macdonald.brett@outlook.com>
2016-10-17 10:07:18 -0700

    added gulp files, made changes for build directory

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    notify = require('gulp-notify'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename = require('gulp-rename'),
+    cssnano = require('gulp-cssnano'),
+    uglify = require('gulp-uglify'),
+    jscs = require('gulp-jscs'),
+    jshint = require('gulp-jshint'),
+    browserSync = require('browser-sync');
+
+var plumberErrorHandler = {
+   errorHandler: notify.onError({
+      title: 'Gulp',
+      message: 'Error: <%= error.message %>'
+   })
+};
+
+gulp.task('sass', function() {
+   gulp.src('./sass/style.scss')
+      .pipe(plumber(plumberErrorHandler))
+      .pipe(sass())
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./build/css'))
+      .pipe(cssnano())
+      .pipe(rename('style.min.css'))
+      .pipe(gulp.dest('./build/css'));
+
+   gulp.src('./sass/style-stretch.scss')
+      .pipe(plumber(plumberErrorHandler))
+      .pipe(sass())
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./build/css'))
+      .pipe(cssnano())
+      .pipe(rename('style-stretch.min.css'))
+      .pipe(gulp.dest('./build/css'));
+});
+
+gulp.task('scripts', function(){
+    gulp.src('./js/*.js')
+      .pipe(uglify())
+      .pipe(rename({
+        extname: '.min.js'
+      }))
+      .pipe(gulp.dest('./build/js'))
+});
+
+gulp.task('jscs', function () {
+   return gulp.src('./js/*.js')
+       .pipe(jscs('.jscsrc'));
+});
+
+gulp.task('lint', function() {
+  return gulp.src('./js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('browser-sync', function() {
+   browserSync.init({
+      server: {
+         baseDir: "./"
+      }
+   });
+
+   gulp.watch(['build/css/*.css', 'build/js/*.js']).on('change', browserSync.reload);
+});
+
+gulp.task('watch', function() {
+   gulp.watch('sass/*.scss', ['sass']);
+   gulp.watch('js/*.js', ['scripts']);
+});
+
+gulp.task('default', ['watch', 'browser-sync']);
